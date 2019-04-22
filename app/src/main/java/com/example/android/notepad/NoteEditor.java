f77b8c53253c8562a2162ee2d0f7144e9320892d
Apple-Jobs <616943145@qq.com>
2019-04-22 13:29:50 +0800

    完成时间戳功能

--- app/src/main/java/com/example/android/notepad/NoteEditor.java
+++ app/src/main/java/com/example/android/notepad/NoteEditor.java
@@ -38,6 +38,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * This Activity handles "editing" a note, where editing is responding to
  * {@link Intent#ACTION_VIEW} (request to view data), edit a note
@@ -525,7 +528,13 @@ public class NoteEditor extends Activity {
 
         // Sets up a map to contain values to be updated in the provider.
         ContentValues values = new ContentValues();
-        values.put(NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE, System.currentTimeMillis());
+
+        Long now = Long.valueOf(System.currentTimeMillis());
+        Date date = new Date(now);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String dateTime = format.format(date);
+
+        values.put(NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE, dateTime);
 
         // If the action is to insert a new note, this creates an initial title for it.
         if (mState == STATE_INSERT) {
